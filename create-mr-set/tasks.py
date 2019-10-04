f8c70d5feb305960cf1ba35aba91fc88a7e432cc
Paul Bond <git@paulsbond.co.uk>
2019-10-04 10:35:24 +0100

    Fix to trim_model Task results contain output paths

--- create-mr-set/tasks.py
+++ create-mr-set/tasks.py
@@ -21,9 +21,11 @@ def _run(executable, args=[], stdin=[], stdout=None, stderr=None):
 
 def mr(hklin, xyzin, identity, prefix, copies, atom_counts):
   """Perform molecular replacement with PHASER"""
-  xyzout = "%s.1.pdb" % prefix
-  stdout = "%s.log" % prefix
-  stderr = "%s.err" % prefix
+  result = {
+    "xyzout": "%s.1.pdb" % prefix,
+    "stdout": "%s.log" % prefix,
+    "stderr": "%s.err" % prefix,
+  }
   keywords = [
     "MODE MR_AUTO",
     "HKLIN %s" % hklin,
@@ -37,9 +39,9 @@ def mr(hklin, xyzin, identity, prefix, copies, atom_counts):
   ]
   for atom in atom_counts:
     keywords.append("COMPOSITION ATOM %-2s NUMBER %d" % (atom, atom_counts[atom]))
-  _run("phaser", stdin=keywords, stdout=stdout, stderr=stderr)
-  if not os.path.exists(xyzout):
-    with open(stdout) as f: log = f.read()
+  _run("phaser", stdin=keywords, stdout=result["stdout"], stderr=result["stderr"])
+  if not os.path.exists(result["xyzout"]):
+    with open(result["stdout"]) as f: log = f.read()
     if not "EXIT STATUS:" in log:
       return { "error": "Early termination" }
     elif "EXIT STATUS: SUCCESS" in log:
@@ -50,67 +52,70 @@ def mr(hklin, xyzin, identity, prefix, copies, atom_counts):
       return { "error": "Bad ensemble given as input" }
     else:
       return { "error": "No coordinates produced" }
-  with open(xyzout) as pdb_file:
+  with open(result["xyzout"]) as pdb_file:
     for line in pdb_file:
       if line[:26] == "REMARK Log-Likelihood Gain":
-        return { "llg": float(line.split()[-1]) }
+        result["llg"] = float(line.split()[-1])
+        return result
 
 
 def refine(hklin, xyzin, prefix, cycles=10):
   """Refine a structure with REFMAC"""
-  hklout = "%s.mtz" % prefix
-  xyzout = "%s.pdb" % prefix
-  xmlout = "%s.xml" % prefix
-  stdout = "%s.log" % prefix
-  stderr = "%s.err" % prefix
+  result = {
+    "hklout": "%s.mtz" % prefix,
+    "xyzout": "%s.pdb" % prefix,
+    "xmlout": "%s.xml" % prefix,
+    "stdout": "%s.log" % prefix,
+    "stderr": "%s.err" % prefix,
+  }
   _run("refmac5", [
     "HKLIN", hklin,
     "XYZIN", xyzin,
-    "HKLOUT", hklout,
-    "XYZOUT", xyzout,
-    "XMLOUT", xmlout,
+    "HKLOUT", result["hklout"],
+    "XYZOUT", result["xyzout"],
+    "XMLOUT", result["xmlout"],
   ], [
     "NCYCLES %d" % cycles,
     "PHOUT",
     "END"
-  ], stdout=stdout, stderr=stderr)
-  with open(stderr) as f:
+  ], stdout=result["stdout"], stderr=result["stderr"])
+  with open(result["stderr"]) as f:
     for line in f:
       if "Refmac:  " in line:
         return { "error": line }
-  for path in (hklout, xyzout, xmlout):
-    if not os.path.exists(path):
-      return { "error": "Output file missing: %s" % path }
-  root = ET.parse(xmlout).getroot()
+  for output in ("hklout", "xyzout", "xmlout"):
+    if not os.path.exists(result[output]):
+      return { "error": "Output file missing: %s" % result[output] }
+  root = ET.parse(result["xmlout"]).getroot()
   rworks = list(root.iter("r_factor"))
   rfrees = list(root.iter("r_free"))
-  return {
-    "final_rfree": float(rfrees[-1].text),
-    "final_rwork": float(rworks[-1].text),
-    "initial_rfree": float(rfrees[0].text),
-    "initial_rwork": float(rworks[0].text),
-  }
+  result["final_rfree"] = float(rfrees[-1].text)
+  result["final_rwork"] = float(rworks[-1].text)
+  result["initial_rfree"] = float(rfrees[0].text)
+  result["initial_rwork"] = float(rworks[0].text)
+  return result
 
 
 def superpose(xyzin1, chain1, xyzin2, chain2, prefix):
   """Superpose one chain over another with GESAMT"""
-  xyzout = "%s.pdb" % prefix
-  seqout = "%s.seq" % prefix
-  stdout = "%s.log" % prefix
-  stderr = "%s.err" % prefix
+  result = {
+    "xyzout": "%s.pdb" % prefix,
+    "seqout": "%s.seq" % prefix,
+    "stdout": "%s.log" % prefix,
+    "stderr": "%s.err" % prefix,
+  }
   _run("gesamt", [
     xyzin1, "-s", "//%s" % chain1,
     xyzin2, "-s", "//%s" % chain2,
-    "-o", xyzout,
-    "-a", seqout,
-  ], stdout=stdout, stderr=stderr)
-  with open(stdout) as f: log = f.read()
+    "-o", result["xyzout"],
+    "-a", result["seqout"],
+  ], stdout=result["stdout"], stderr=result["stderr"])
+  with open(result["stdout"]) as f: log = f.read()
   if "DISSIMILAR and cannot be reasonably aligned" in log:
     return { "error": "GESAMT: Query and target are too dissimilar" }
-  for path in (xyzout, seqout):
-    if not os.path.exists(path):
-      return { "error": "Output file missing: %s" % path }
-  result = {}
+  for output in ("xyzout", "seqout"):
+    if not os.path.exists(result[output]):
+      return { "error": "Output file missing: %s" % result[output] }
   match = re.search(r" Q-score          : (\d+\.\d+)", log)
   result["qscore"] = float(match.group(1))
   match = re.search(r" RMSD             : (\d+\.\d+)", log)
@@ -123,11 +128,12 @@ def superpose(xyzin1, chain1, xyzin2, chain2, prefix):
 
 
 def trim_model(model, chain, alignment, prefix):
-  """Trim an MR model using SCULPTOR"""
-  tmpout = "tmp%s" % uuid.uuid4()
-  xyzout = "%s.pdb" % prefix
-  stdout = "%s.log" % prefix
-  stderr = "%s.err" % prefix
+  """Trim a molecular replacement model using SCULPTOR"""
+  result = {
+    "xyzout": "%s_%s.pdb" % (prefix, os.path.basename(model).split(".")[0]),
+    "stdout": "%s.log" % prefix,
+    "stderr": "%s.err" % prefix,
+  }
   _run("phaser.sculptor", ["--stdin"], [
     "input {",
     "  model {"
@@ -142,20 +148,17 @@ def trim_model(model, chain, alignment, prefix):
     "}",
     "output {",
     "  folder = %s" % os.path.dirname(prefix),
-    "  root = '%s'" % tmpout,
+    "  root = '%s'" % os.path.basename(prefix),
     "}",
-  ], stdout=stdout, stderr=stderr)
-  files = os.listdir(os.path.dirname(prefix))
-  tmpout = [f for f in files if tmpout in f]
-  tmpout = os.path.join(os.path.dirname(prefix), tmpout)
-  if not os.path.exists(tmpout):
+  ], stdout=result["stdout"], stderr=result["stderr"])
+  if not os.path.exists(result["xyzout"]):
     return { "error": "No trimmed coordinates produced" }
-  os.rename(tmpout, xyzout)
   has_atoms = False
-  with open(xyzout) as f:
+  with open(result["xyzout"]) as f:
     for line in f:
       if line[:6] == "ATOM  ":
         has_atoms = True
         break
   if not has_atoms:
     return { "error": "SCULPTOR: No atoms in trimmed coordinates" }
+  return result
